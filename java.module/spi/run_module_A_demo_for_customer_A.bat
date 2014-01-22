5141f0ed41c69ac4e1a95ab8132380f4205ac2b7
imanolache <manolachei@yahoo.com>
2014-01-22 14:23:33 +0200

    added bat files

---
+++ java.module/spi/run_module_A_demo_for_customer_A.bat
@@ -0,0 +1,4 @@
+set startdir=%cd%
+set CLASSPATH=%startdir%\module_A_demo\target\module_A_demo-1.0.0-SNAPSHOT.jar;%startdir%\module_A\target\module_A-1.0.0-SNAPSHOT.jar;%startdir%\module_A_customer_A\target\module_A_customer_A-1.0.0-SNAPSHOT.jar
+
+java ro.ima.kb.hello.ModuleADemo
